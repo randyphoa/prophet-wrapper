b7584874067576b58d0a58c8252de0a98c61588f
randyphoa <randyphoa8@gmail.com>
2021-12-08 01:47:35 +0800

    -

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # prophet-wrapper
+
+python -m pip install --upgrade build
+python -m pip install --upgrade twine
+
+python -m build
+python -m twine upload dist/*
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name="Prophet Wrapper",
+    name="prophet_wrapper",
     version="0.1",
     description="A simple wrapper for Prophet",
     url="https://github.com/",
     author="Randy Phoa",
     author_email="randy.phoa@ibm.com",
     license="MIT",
-    packages=["prophet_wrapper"],
+    packages=find_packages(),
     # install_requires=["Django >= 1.1.1", "pytest",],
 )
